abf7a1ae844367ba381b82c82e156366b599e28f
sormuras <sormuras@gmail.com>
2015-02-15 21:34:56 +0100

    Web listener is now master.

--- src/server/de/stonebone/cars/server/CarsEventSource.java
+++ src/server/de/stonebone/cars/server/CarsEventSource.java
@@ -1,81 +1,29 @@
 package de.stonebone.cars.server;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
 
-import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import de.stonebone.cars.ControllerState;
-
 @WebServlet(urlPatterns = "/event", asyncSupported = true)
-public class CarsEventSource extends HttpServlet implements Runnable {
+public class CarsEventSource extends HttpServlet {
 
   private static final long serialVersionUID = 1L;
 
-  private StringBuilder builder = new StringBuilder();
-
-  private int counter = 0;
-
-  private Set<AsyncContext> contexts = new HashSet<>();
-
   @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     response.setContentType("text/event-stream");
+
     response.setCharacterEncoding("UTF-8");
-    response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
-    response.setHeader("Pragma", "no-cache"); // HTTP 1.0.
-    response.setDateHeader("Expires", 0); // Proxies.
+    response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    response.setHeader("Pragma", "no-cache");
+    response.setDateHeader("Expires", 0);
 
-    
-    AsyncContext ac = req.startAsync();
-    contexts.add(ac);
-    
-    Server server = (Server) getServletContext().getAttribute("server");
-    server.setSocketTimeoutListener(this);
-  }
-
-  public void run() {
-    Server server = (Server) getServletContext().getAttribute("server");
-
-    builder.setLength(0);
-    builder.append("data:");
-    builder.append(++counter);
-
-    builder.append("/").append(server);
-    builder.append("/").append(server.getServerState());
-    builder.append("/").append(server.getServerState().getSerial());
-    builder.append("/").append(server.getServerState().getControllers()[0].getSerial());
-
-    ControllerState[] cons = server.getServerState().getControllers();
-    for (int id = 0; id < cons.length; id++) {
-      builder.append("<br>").append(id);
-      builder.append("=").append(cons[id].toString());
-    }
-
-    builder.append("\n\n");
-
-    String s = builder.toString();
-
-    Iterator<AsyncContext> iterator = contexts.iterator();
-    while (iterator.hasNext()) {
-      AsyncContext context = iterator.next();
-      try {
-        PrintWriter writer = context.getResponse().getWriter();
-        writer.write(s);
-        writer.flush();
-      } catch (IOException e) {
-        iterator.remove();
-      }
-    }
-
+    CarsWebListener cars = (CarsWebListener) getServletContext().getAttribute("cars");
+    cars.getAsyncContexts().add(request.startAsync(request, response));
   }
 
 }
--- src/server/de/stonebone/cars/server/CarsWebListener.java
+++ src/server/de/stonebone/cars/server/CarsWebListener.java
@@ -1,31 +1,81 @@
 package de.stonebone.cars.server;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
+import javax.servlet.AsyncContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
+import de.stonebone.cars.ControllerState;
+
 @WebListener
-public class CarsWebListener implements ServletContextListener, Serializable {
+public class CarsWebListener implements Runnable, ServletContextListener, Serializable {
 
   private static final long serialVersionUID = 1L;
 
+  private final Set<AsyncContext> asyncContexts = new HashSet<>();
+  private final StringBuilder builder = new StringBuilder();
+  private int counter = 0;
+  private final Server server = new Server();
+
   @Override
   public void contextDestroyed(ServletContextEvent event) {
-    Server server = (Server) event.getServletContext().getAttribute("server");
     server.close();
   }
 
   @Override
   public void contextInitialized(ServletContextEvent event) {
-
-    Server server = new Server();
-    event.getServletContext().setAttribute("server", server);
+    event.getServletContext().setAttribute("cars", this);
 
     Thread worker = new Thread(server);
     worker.setDaemon(true);
     worker.start();
+
+    server.setSocketTimeoutListener(this);
+  }
+
+  public Set<AsyncContext> getAsyncContexts() {
+    return asyncContexts;
+  }
+
+  public void run() {
+    builder.setLength(0);
+    builder.append("data:");
+    builder.append(++counter);
+
+    builder.append("/").append(server);
+    builder.append("/").append(server.getServerState());
+    builder.append("/").append(server.getServerState().getSerial());
+    builder.append("/").append(server.getServerState().getControllers()[0].getSerial());
+
+    ControllerState[] cons = server.getServerState().getControllers();
+    for (int id = 0; id < cons.length; id++) {
+      builder.append("<br>").append(id);
+      builder.append("=").append(cons[id].toString());
+    }
+
+    builder.append("\n\n");
+
+    String s = builder.toString();
+
+    Iterator<AsyncContext> iterator = getAsyncContexts().iterator();
+    while (iterator.hasNext()) {
+      AsyncContext context = iterator.next();
+      try {
+        PrintWriter writer = context.getResponse().getWriter();
+        writer.write(s);
+        writer.flush();
+      } catch (IOException e) {
+        iterator.remove();
+      }
+    }
+
   }
 
 }
